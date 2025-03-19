938e78a9f1998b73218d1115c36920fc140bb0a5
Polly Hsu <pollylearnhsu@gmail.com>
2025-03-19 17:57:37 +0800

    fix(cors): 路徑加上storage/

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
+namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\orderController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -17,3 +17,7 @@ Route::get('/order/{order_id}', [OrderController::class, 'getOrder']);
 Route::get('/users', [UserController::class, 'index']); // 取得所有用戶
 Route::get('/users/{id}', [UserController::class, 'show']); // 取得特定用戶
 Route::get('/users/{id}/orders', [UserController::class, 'getUserOrders']);//獲取order_main資料
+
+
+Route::get('/banners', [StoreController::class, 'index']);
+Route::post('/banners/{id}', [StoreController::class, 'update']); // 確保這裡是 POST
